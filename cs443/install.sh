7d60425045826f0a23ed7d71124354e9bdee4cbc
Ian Murray Wood <ianwood2024@u.northwestern.edu>
2023-04-24 19:31:20 -0500

    remove cs433 folder and add run-nautilus.sh

--- cs443/install.sh
+++
@@ -1,3 +0,0 @@
-qemu-system-x86_64 -enable-kvm -cpu Nehalem  -m 4G -vga virtio -display default -machine type=q35 -smp 2 -cdrom mini.iso -drive file=disk.qcow2,if=virtio -device virtio-net,netdev=mynet0 -netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9
-
-
--- cs443/setup.sh
+++
@@ -1,3 +0,0 @@
-wget http://archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/mini.iso
-qemu-img create -f qcow2 disk.qcow2 20G
-
